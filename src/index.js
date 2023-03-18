6f0641150503d2832deec21c24bcd7df2cdb22aa
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-03-18 21:51:15 +0100

    installed styled-components, created theme and globalStyle file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
+import reportWebVitals from "./reportWebVitals";
+import { GlobalStyle } from "./GlobalStyle";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    tango: "#F16718",
+    atomicTangerine: "#FF9B62",
+    bigStone: "#162542",
+    wildBlueYonder: "#7B8BAD",
+    mystic: "#E8EFF2",
+  },
+  breakpoint: {},
+};
